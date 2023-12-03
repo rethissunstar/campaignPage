969fa7f92d3c0e9a1cfc6bbd91ae7fde0ac8c997
rethissunstar <rethissunstar@gmail.com>
2023-12-03 17:23:27 -0500

    bones complete

---
+++ songsofshadow/src/components/charms/charms.jsx
@@ -0,0 +1,8 @@
+// Example content for Home.js, Story.js, Characters.js, Charms.js
+import React from "react";
+
+const Home = () => {
+  return <div>Home Component</div>;
+};
+
+export default Home;
